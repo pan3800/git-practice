7b1f54b55fc40eccc4899fb24c01b46032756a57
pan3800 <pan173434@gmail.com>
2024-07-03 16:24:11 +0900

    브랜치 커밋

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
 print("hello")
-
-print("hello world, good bye, See you soon!")
+print("hello world, good bye, See you again")
 print("test1, hello")
 print("test1, goodbye")
-print("test2, Hi!!")
-
